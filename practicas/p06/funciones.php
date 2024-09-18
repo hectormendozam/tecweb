d86497c33b2cf5363a994cf0b74289cb4be10731
Hector Mendoza <hector.mendozam@alumno.buap.mx>
2024-09-18 15:36:27 -0600

    p06 - ejercicio 1 - multiplo de 5 y 7

---
+++ practicas/p06/funciones.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 6</title>
+</head>
+<body>
+    <?php
+    function esMultiplo($num){
+        if(isset($_GET['numero']))
+        {
+        $num = $_GET['numero'];
+        if ($num%5==0 && $num%7==0)
+        {
+            echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
+        }
+        else
+        {
+            echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
+        }
+        }
+    }
+    ?>
+</body>
+</html>
---
+++ practicas/p06/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 6</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    <form> 
+        <input type="text" name="numero" placeholder="Ingrese un número">
+        <input type="submit" value="Comprobar">
+    </form>
+    <?php
+    include 'funciones.php';
+    esMultiplo(0);
+    ?>
+
+    <h2>Ejemplo de POST</h2>
+    <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+    <br>
+    <?php
+        if(isset($_POST["name"]) && isset($_POST["email"]))
+        {
+            echo $_POST["name"];
+            echo '<br>';
+            echo $_POST["email"];
+        }
+    ?>
+</body>
+</html>
