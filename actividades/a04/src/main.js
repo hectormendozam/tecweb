dc0cb029863fc4419dd00114ee3f6d1fcf8925a4
Hector Mendoza <hector.mendozam@alumno.buap.mx>
2024-10-08 18:42:10 -0600

    a04 - create functions from JS03

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -41,3 +41,106 @@ function ingresarDatos(){
     document.write(edad);
     document.write(' años');
     }
+
+function sumaYProducto() {
+    var valor1;
+    var valor2;
+    valor1 = prompt('Introducir primer número:', '');
+    valor2 = prompt('Introducir segundo número', '');
+    var suma = parseInt(valor1)+parseInt(valor2);
+    var producto = parseInt(valor1)*parseInt(valor2);
+    document.write('La suma es ');
+    document.write(suma);
+    document.write('<br>');
+    document.write('El producto es ');
+    document.write(producto);
+}
+
+function aprobado (){
+    var nombre;
+    var nota;
+    nombre = prompt('Ingresa tu nombre:', '');
+    nota = prompt('Ingresa tu nota:', '');
+    if (nota>=4) {
+    document.write(nombre+' esta aprobado con un '+nota);
+    }
+}
+
+function numeroMayor (){
+    var num1,num2;
+    num1 = prompt('Ingresa el primer número:', '');
+    num2 = prompt('Ingresa el segundo número:', '');
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    if (num1>num2) {
+    document.write('el mayor es '+num1);
+    }
+    else {
+    document.write('el mayor es '+num2);
+    }
+}
+
+function AprobadoReprobado(){
+    var nota1,nota2,nota3;
+
+    nota1 = prompt('Ingresa 1ra. nota:', '');
+    nota2 = prompt('Ingresa 2da. nota:', '');
+    nota3 = prompt('Ingresa 3ra. nota:', '');
+
+    //Convertimos los 3 string en enteros
+    nota1 = parseInt(nota1);
+    nota2 = parseInt(nota2);
+    nota3 = parseInt(nota3);
+
+    var pro;
+    pro = (nota1+nota2+nota3)/3;
+}
+
+function deUnoACinco () {
+    var valor;
+    valor = prompt('Ingresar un valor comprendido entre 1 y 5:', '' );
+    //Convertimos a entero
+    valor = parseInt(valor);
+    switch (valor) {
+    case 1: document.write('uno');
+
+    break;
+
+    case 2: document.write('dos');
+
+    break;
+
+    case 3: document.write('tres');
+
+    break;
+
+    case 4: document.write('cuatro');
+
+    break;
+
+    case 5: document.write('cinco');
+
+    break;
+
+    default:document.write('debe ingresar un valor comprendido entre 1 y 5.');
+}
+}
+
+function colores (){
+    var col;
+    col = prompt('Ingresa el color con que quierar pintar el fondo de la ventana (rojo, verde, azul)' , '' );
+    switch (col) {
+    case 'rojo': document.bgColor='#ff0000';
+
+    break;
+
+    case 'verde': document.bgColor='#00ff00';
+
+    break;
+
+    case 'azul': document.bgColor='#0000ff';
+
+    break;
+
+    }
+}
