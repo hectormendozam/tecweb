ca3f3de807358a63643c6354623bab5e3521b89c
Hector Mendoza <hector.mendozam@alumno.buap.mx>
2024-10-22 20:35:19 -0600

    p11 - paso 3 - creacion de funcion listarProductos con JQuery

--- practicas/p11/p11_con_jquery/product_app/app.js
+++ practicas/p11/p11_con_jquery/product_app/app.js
@@ -19,3 +19,46 @@ function init() {
     // SE LISTAN TODOS LOS PRODUCTOS
     listarProductos();
 }
+
+function listarProductos() {
+    $.ajax({
+        url: './backend/product-list.php',
+        method: 'GET',
+        dataType: 'json',
+        success: function(productos) {
+            if (productos.length > 0) {
+                let template = '';
+                let template_bar = '';
+
+                productos.forEach(producto => {
+                    let descripcion = `
+                        <li>precio: ${producto.precio}</li>
+                        <li>unidades: ${producto.unidades}</li>
+                        <li>modelo: ${producto.modelo}</li>
+                        <li>marca: ${producto.marca}</li>
+                        <li>detalles: ${producto.detalles}</li>
+                    `;
+
+                    template += `
+                        <tr productId="${producto.id}">
+                            <td>${producto.id}</td>
+                            <td>${producto.nombre}</td>
+                            <td><ul>${descripcion}</ul></td>
+                            <td>
+                                <button class="product-delete btn btn-danger" onclick="eliminarProducto(${producto.id})">
+                                    Eliminar
+                                </button>
+                            </td>
+                        </tr>
+                    `;
+                    template_bar += `<li>${producto.nombre}</li>`;
+                });
+
+                $("#products").html(template);
+                $("#container").html(template_bar);
+                $("#product-result").addClass("d-block"); // Mostrar barra de estado
+            }
+        }
+    });
+}
+
