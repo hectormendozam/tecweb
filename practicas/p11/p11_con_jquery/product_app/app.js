833e35c6e9d1918e10a0f7e3da191b92bfbeaa1f
Hector Mendoza <hector.mendozam@alumno.buap.mx>
2024-10-22 20:36:17 -0600

    p11 - paso 3 - creacion de funcion buscarProductos con JQuery

--- practicas/p11/p11_con_jquery/product_app/app.js
+++ practicas/p11/p11_con_jquery/product_app/app.js
@@ -62,3 +62,47 @@ function listarProductos() {
     });
 }
 
+function buscarProducto() {
+    var search = $('#search').val();
+    $.ajax({
+        url: './backend/product-search.php',
+        method: 'GET',
+        data: { search: search },
+        dataType: 'json',
+        success: function(productos) {
+            if (productos.length > 0) {
+                let template = '';
+                let template_bar = '';
+
+                productos.forEach(producto => {
+                    let descripcion = `
+                        <li>precio: ${producto.precio}</li>
+                        <li>unidades: ${producto.unidades}</li>
+                        <li>modelo: ${producto.modelo}</li>
+                        <li>marca: ${producto.marca}</li>
+                        <li>detalles: ${producto.detalles}</li>
+                    `;
+
+                    template += `
+                        <tr productId="${producto.id}">
+                            <td>${producto.id}</td>
+                            <td>${producto.nombre}</td>
+                            <td><ul>${descripcion}</ul></td>
+                            <td>
+                                <button class="product-delete btn btn-danger" onclick="eliminarProducto(${producto.id})">
+                                    Eliminar
+                                </button>
+                            </td>
+                        </tr>
+                    `;
+                    template_bar += `<li>${producto.nombre}</li>`;
+                });
+
+                $("#product-result").addClass("d-block");
+                $("#container").html(template_bar);
+                $("#products").html(template);
+            }
+        }
+    });
+}
+
